4d13793ca65e656d8f126b2fe2d3542ff1424869
subham sarkar <subhamsarkar@subhams-MacBook-Air.local>
2023-03-06 23:30:00 +0530

    done till pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -11,7 +11,8 @@ from flaskblog.models import User, Post
 @app.route("/")
 @app.route("/home")
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type= int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page= page,per_page=5)
     return render_template('home.html', posts= posts)
 
 @app.route("/about")
@@ -123,6 +124,7 @@ def post(post_id):
     return render_template('post.html', tite= post.title, post=post)
 
 
+# for updating the post
 @app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
 @login_required
 def update_post(post_id):
@@ -143,3 +145,29 @@ def update_post(post_id):
     return render_template('create_post.html', title= 'Update',
                             form= form, legend=' Update Post')
 
+
+
+
+
+# for deleting the post
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page', 1, type= int)
+    user = User.query.filter_by(username=username).first_or_404()
+    
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page= page,per_page=5)
+    return render_template('user_post.html', posts= posts, user=user)
