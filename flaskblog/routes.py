7ea8e6340680c0d9b741e2d37ec0a663f0b3da7b
subham sarkar <subhamsarkar@subhams-MacBook-Air.local>
2023-03-06 15:14:34 +0530

    takes posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,36 +1,17 @@
-
-from flask import render_template , url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm , LoginForm
+import os
+import secrets 
+from PIL import Image
+from flask import render_template , url_for, flash, redirect, request, abort
+from flaskblog.forms import RegistrationForm , LoginForm, UpdateAccountForm , PostForm
 from flaskblog import app, db, bcrypt
 from flask_login import login_user,logout_user, current_user, logout_user, login_required
 from flaskblog.models import User, Post
 
-posts =[ 
-    {
-    'author':'Subham Sarkar',
-    'title':' the alchemist',
-    'content':'your lie in April',
-    'date_processed':'10th of the April'
-    },
-    {
-    'author':'Subham Sarkar',
-    'title':' the alchemist',
-    'content':'your lie in April',
-    'date_processed':'10th of the April'
-    
-    },
-    {
-    'author':'Subham Sarkar',
-    'title':' the helloo',
-    'content':'your lie in April',
-    'date_processed':'10th of the April'
-    
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts= posts)
 
 @app.route("/about")
@@ -77,7 +58,88 @@ def logout():
     return redirect(url_for('home'))
 
 
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename )
+    picture_fn = random_hex +f_ext
+    picture_path = os.path.join(app.root_path, 'static/images', picture_fn)
+
+    output_size = (125, 110)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+   
+    
+    return picture_fn
+
+
+
+@app.route("/account", methods = ['GET', 'POST'])
 @login_required 
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file= picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(' your account have been updated!', 'sucess')
+        return redirect(url_for('account'))
+    
+    elif request.method =='GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='images/'+ current_user.image_file)
+    return render_template('account.html', 
+                           title='Account', image_file= image_file, form= form)
+
+
+
+@app.route("/post/new", methods = ['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post= Post(title= form.title.data, content= form.content.data, author= current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash ('your post has been created', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('create_post.html', title= 'New Post', 
+                            form= form ,legend=' New Post')
+                           
+
+
+
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', tite= post.title, post=post)
+
+
+@app.route("/post/<int:post_id>/update", methods = ['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()  
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('your post have been updated!' , 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+
+        form.title.data = post.title 
+        form.content.data = post.content
+    return render_template('create_post.html', title= 'Update',
+                            form= form, legend=' Update Post')
+
